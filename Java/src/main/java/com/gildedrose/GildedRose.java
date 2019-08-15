4ce7d9b735b91ba0d3af88a300baaffbc3cc449b
nicole.budiman <nicole.budiman@thoughtworks.com>
2019-08-15 15:08:22 +0800

    Refactor and implement Conjured items

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -7,55 +7,68 @@ class GildedRose {
         this.items = items;
     }
 
+    private void updateSellIn(Item item) {
+        item.sellIn -= 1;
+    }
+
+    private void updateQuality(Item item, int rate) {
+        item.quality += rate;
+    }
+
+    private void updateAgedBrie(Item item) {
+        updateSellIn(item);
+        if (item.quality < 50) {
+            if (item.sellIn <=0) {
+                updateQuality(item, 1);
+            }
+            updateQuality(item, 1);
+        }
+    }
+
+    private void updateBackstagePass(Item item) {
+        updateSellIn(item);
+        if (item.quality < 50) {
+            updateQuality(item, 1);
+            if (item.sellIn <= 10) {
+                updateQuality(item, 1);
+            }
+            if (item.sellIn <= 5) {
+                updateQuality(item, 1);
+            }
+        }
+        if (item.sellIn <= 0) {
+            item.quality = 0;
+        }
+    }
+
+    private void updateOtherItem(Item item) {
+        updateSellIn(item);
+        if (item.sellIn <= 0) {
+            updateQuality(item, -1);
+        }
+        updateQuality(item, -1);
+    }
+
+    private void updateConjured(Item item) {
+        updateSellIn(item);
+        if (item.sellIn <= 0) {
+            updateQuality(item, -2);
+        }
+        updateQuality(item, -2);
+    }
+
     public void updateQuality() {
-        for (int i = 0; i < items.length; i++) {
-            if (!items[i].name.equals("Aged Brie")
-                    && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (items[i].quality > 0) {
-                    if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                        items[i].quality = items[i].quality - 1;
-                    }
-                }
+        for (Item currentItem : items) {
+            if (currentItem.name.startsWith("Sulfuras")) {
+                continue;
+            } else if (currentItem.name.equals("Aged Brie")) {
+                updateAgedBrie(currentItem);
+            } else if (currentItem.name.startsWith("Backstage pass")) {
+                updateBackstagePass(currentItem);
+            } else if (currentItem.name.startsWith("Conjured")) {
+                updateConjured(currentItem);
             } else {
-                if (items[i].quality < 50) {
-                    items[i].quality = items[i].quality + 1;
-
-                    if (items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].sellIn < 11) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-
-                        if (items[i].sellIn < 6) {
-                            if (items[i].quality < 50) {
-                                items[i].quality = items[i].quality + 1;
-                            }
-                        }
-                    }
-                }
-            }
-
-            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                items[i].sellIn = items[i].sellIn - 1;
-            }
-
-            if (items[i].sellIn < 0) {
-                if (!items[i].name.equals("Aged Brie")) {
-                    if (!items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                        if (items[i].quality > 0) {
-                            if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
-                                items[i].quality = items[i].quality - 1;
-                            }
-                        }
-                    } else {
-                        items[i].quality = items[i].quality - items[i].quality;
-                    }
-                } else {
-                    if (items[i].quality < 50) {
-                        items[i].quality = items[i].quality + 1;
-                    }
-                }
+                updateOtherItem(currentItem);
             }
         }
     }
--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,119 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void SulfurasQualityShouldRemain80() {
+        Item[] items = new Item[] { new Item("Sulfuras, Hand of Ragnaros", 0, 80) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals( 0, app.items[0].sellIn);
+        assertEquals(80, app.items[0].quality);
     }
 
+    @Test
+    public void AgedBrieShouldIncreaseQualityBy1UntilSellByDate() {
+        Item[] items = new Item[] { new Item("Aged Brie", 2, 1) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals( 1, app.items[0].sellIn);
+        assertEquals(2, app.items[0].quality);
+    }
+
+    @Test
+    public void AgedBrieShouldIncreaseQualityBy2AfterSellByDate() {
+        Item[] items = new Item[] { new Item("Aged Brie", 0, 5) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals( -1, app.items[0].sellIn);
+        assertEquals(7, app.items[0].quality);
+    }
+
+    @Test
+    public void AgedBrieShouldNotIncreaseQualityOver50() {
+        Item[] items = new Item[] { new Item("Aged Brie", 10, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals( 9, app.items[0].sellIn);
+        assertEquals(50, app.items[0].quality);
+    }
+
+    @Test
+    public void BackstagePassShouldIncreaseQualityBy3WhenSellByDateLessThan5() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 5, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals( 4, app.items[0].sellIn);
+        assertEquals(13, app.items[0].quality);
+    }
+
+    @Test
+    public void BackstagePassShouldIncreaseQualityBy2WhenSellByDateLessThan10() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals( 9, app.items[0].sellIn);
+        assertEquals(12, app.items[0].quality);
+    }
+
+    @Test
+    public void BackstagePassShouldIncreaseQualityBy1WhenSellByDateMoreThan10() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 12, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals( 11, app.items[0].sellIn);
+        assertEquals(11, app.items[0].quality);
+    }
+
+    @Test
+    public void BackstagePassShouldHaveQuality0WhenSellByDateLessThan0() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals( -1, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+    }
+
+    @Test
+    public void BackstagePassShouldNotIncreaseQualityOver50() {
+        Item[] items = new Item[] { new Item("Backstage passes to a TAFKAL80ETC concert", 10, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals(50, app.items[0].quality);
+        assertEquals( 9, app.items[0].sellIn);
+    }
+
+    @Test
+    public void OtherItemShouldDecreaseQualityBy1TillSellByDate() {
+        Item[] items = new Item[] { new Item("Other", 10, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals( 9, app.items[0].sellIn);
+        assertEquals(49, app.items[0].quality);
+    }
+
+    @Test
+    public void OtherItemShouldDecreaseQualityBy2AfterSellByDate() {
+        Item[] items = new Item[] { new Item("Other", 0, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals( -1, app.items[0].sellIn);
+        assertEquals(48, app.items[0].quality);
+    }
+
+    @Test
+    public void ConjuredShouldDecreaseQualityBy2BeforeSellByDate() {
+        Item[] items = new Item[] { new Item("Conjured", 10, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals( 9, app.items[0].sellIn);
+        assertEquals(48, app.items[0].quality);
+    }
+
+    @Test
+    public void ConjuredShouldDecreaseQualityBy4AfterSellByDate() {
+        Item[] items = new Item[] { new Item("Conjured", 0, 50) };
+        GildedRose app = new GildedRose(items);
+        app.updateQuality();
+        assertEquals( -1, app.items[0].sellIn);
+        assertEquals(46, app.items[0].quality);
+    }
 }
